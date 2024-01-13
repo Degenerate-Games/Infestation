fb0a30536823735aafbb031deb3d1cf7a481868f
James Wolff <james.wolff0@gmail.com>
2024-01-12 23:51:01 -0600

    Add Global autoload

---
+++ scripts/global.gd
@@ -0,0 +1,7 @@
+extends Node
+
+func lerpvec2(from: Vector2, to: Vector2, weight: float):
+	var temp = Vector2.ZERO
+	temp.x = lerpf(from.x, to.x, weight)
+	temp.y = lerpf(from.y, to.y, weight)
+	return temp
