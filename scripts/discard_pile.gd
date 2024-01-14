c53d26b28c7b012a07a49658da3fa2642f7eac02
James Wolff <james.wolff0@gmail.com>
2024-01-13 21:34:50 -0600

    Add discard_pile to HUD

--- scripts/discard_pile.gd
+++ scripts/discard_pile.gd
@@ -1,26 +1,18 @@
-extends Control
+extends Marker2D
 
-@export_category("Card Data")
-@export var card_name: String = "Card Name":
-	get:
-		return card_name
-	set(value):
-		if get_node("CardName"):
-			$CardName.text = value
-		card_name = value
-@export var art_texture: Texture2D = null:
-	get:
-		return art_texture
-	set(value):
-		if get_node("Art"):
-			$Art.texture = value
-		art_texture = value
+var discard_pile: Array[Control]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if $CardName: $CardName.text = card_name
-	if $Art: $Art.texture = art_texture
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func discard_card(card):
+	card.destination = global_position
+	card.hoverable = false
+	discard_pile.append(card)
+	add_child(card)
