36dc5e8d849f5315ec399fe6e419ec83da2528a9
James Wolff <james.wolff0@gmail.com>
2024-01-15 20:18:23 -0600

    Change HUD variable

--- scripts/global.gd
+++ scripts/global.gd
@@ -3,11 +3,7 @@ extends Node
 var selected_card
 var HUD: Node2D:
 	get:
-		var hud = get_node("/root/Level/HUD")
-		if hud:
-			return hud
-		else:
-			return null
+		return get_node_or_null("/root/Level/HUD")
 
 func lerpvec2(from: Vector2, to: Vector2, weight: float):
 	var temp = Vector2.ZERO
