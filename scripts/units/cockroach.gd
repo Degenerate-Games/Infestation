825331cd019649888e7f04433ca62f0f7a25cae7
James Wolff <james.wolff0@gmail.com>
2024-01-15 20:16:56 -0600

    Add scenes for units

---
+++ scripts/units/cockroach.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+var target: Node2D
+var paused: bool = false
+
+func _ready():
+	motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
+	pass
+
+func _physics_process(delta):
+	if paused: return
+	velocity = pick_direction() * SPEED
+	print(velocity)
+	move_and_slide()
+
+func pick_direction():
+	var random_direction = Vector2(randf_range(-1.0, 1.0), randf_range(-1.0, 1.0))
+	if target:
+		random_direction = random_direction.dot(target.position)
+	return random_direction
