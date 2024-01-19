ce16ab9ffeea1c8f0f0f776ef755edb83bb8127a
James Wolff <james.wolff0@gmail.com>
2024-01-18 22:28:13 -0600

    Rename paused and add spawn_delay function

--- scripts/units/unit.gd
+++ scripts/units/unit.gd
@@ -4,22 +4,25 @@ class_name Unit
 
 const SPEED = 300.0
 var target: Node2D
-var paused: bool = false
+var moving: bool = false
 var attack
 var defense
+@export var spawn_timer: Timer
 
-func _ready():
-	motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
-	pass
-
-func _physics_process(delta):
-	if paused: return
-	velocity = pick_direction() * SPEED * delta
-	print(velocity)
-	move_and_slide()
+func _physics_process(_delta):
+	if moving:
+		velocity = pick_direction() * SPEED
+		# print(velocity)
+		move_and_slide()
 
 func pick_direction():
 	var random_direction = Vector2(randf_range(-1.0, 1.0), randf_range(-1.0, 1.0))
 	if target:
 		random_direction = random_direction.dot(target.position)
 	return random_direction
+
+func delayed_spawn(delay):
+	spawn_timer.start(delay)
+	await spawn_timer.timeout
+	# TODO:: Teleport to spawn point
+	moving = true
