9c9b89ae7c38b4607b1122751a06f76036c7ac53
James Wolff <james.wolff0@gmail.com>
2024-01-22 21:35:47 -0600

    Add node collision and handle selecting target

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Node2D
 
 var selected_card
 var current_level: Node2D:
@@ -18,3 +18,35 @@ func get_random_target():
 	var targets = current_level.get_node("Enemies/Structures").get_children()
 	var target = targets[randi() % targets.size()]
 	return target
+
+func get_physics_node_under_mouse():
+	var mouse_position = get_viewport().get_mouse_position()
+	var space_state = get_world_2d().direct_space_state
+	var query = PhysicsPointQueryParameters2D.new()
+	query.position = mouse_position
+	query.collision_mask = Global.get_layer_mask([2,3])
+	var result = space_state.intersect_point(query)
+	if result:
+			return result[0]["collider"]
+	return null
+
+func get_layer_mask(layers):
+	var mask = 0
+	for layer in layers:
+		mask += pow(2, layer - 1)
+	return mask
+
+func _unhandled_input(event):
+	if event.is_action_pressed("Select"):
+		print("Select")
+		if selected_card:
+			print(selected_card)
+			if selected_card.play_actions.has("manual_target"):
+				print("manual_target")
+				var node = get_physics_node_under_mouse()
+				if node != null:
+					selected_card.play_actions["manual_target"] = node
+					selected_card.selected = false
+					selected_card.play_card()
+					HUD.discard_card(selected_card)
+					selected_card = null
