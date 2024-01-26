8d9997a5dfe7eef69428c9f91b6098615c40d237
James Wolff <james.wolff0@gmail.com>
2024-01-25 21:17:22 -0600

    Add health bar to enemy troops

--- scripts/enemies/troop.gd
+++ scripts/enemies/troop.gd
@@ -3,5 +3,11 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
+@export var health_bar: Control
+
 func _physics_process(_delta):
 	move_and_slide()
+
+func take_damage(damage):
+	if health_bar.take_damage(damage):
+		queue_free()
