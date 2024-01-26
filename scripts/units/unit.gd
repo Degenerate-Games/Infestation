2d7de7a353e79bea39c5d5dba28b234693680c1d
James Wolff <james.wolff0@gmail.com>
2024-01-25 20:59:20 -0600

    Add health bar to units

--- scripts/units/unit.gd
+++ scripts/units/unit.gd
@@ -8,12 +8,15 @@ var moving: bool = false
 var attack
 var defense
 @export var spawn_timer: Timer
+@export var health_bar: Control
 
 func _physics_process(delta):
 	if moving:
 		velocity = pick_direction() * SPEED
+		var old_rot = rotation
 		rotation = rotate_toward(rotation, velocity.angle(), delta)
 		move_and_slide()
+		health_bar.rotation -= rotation - old_rot
 
 func pick_direction():
 	var random_direction = Vector2(randf_range(-1.0, 1.0), randf_range(-1.0, 1.0))
@@ -32,3 +35,7 @@ func delayed_spawn(delay, trgt = null):
 		target = trgt	
 	reparent(Global.current_level.get_node("Spawner"), false)
 	moving = true
+
+func take_damage(damage):
+	if health_bar.take_damage(damage):
+		queue_free()
