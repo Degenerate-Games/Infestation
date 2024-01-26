8868add739ce085871cdfeec35d0132a3137d279
James Wolff <james.wolff0@gmail.com>
2024-01-26 17:38:49 -0600

    Delete unused unit scripts

--- scripts/units/mouse.gd
+++
@@ -1,8 +0,0 @@
-extends Unit
-
-func _ready():
-  %MouseSprite.stop()
-
-func delayed_spawn(delay, trgt = null):
-  await super(delay, trgt)
-  %MouseSprite.play("default")
--- scripts/units/rat.gd
+++
@@ -1,8 +0,0 @@
-extends Unit
-
-func _ready():
-  %RatSprite.stop()
-
-func delayed_spawn(delay, trgt = null):
-  await super(delay, trgt)
-  %RatSprite.play("default")
