224dcebf3da5f226734a9106618362d11dde1315
James Wolff <james.wolff0@gmail.com>
2024-01-25 20:12:47 -0600

    Update health bar script and scene

---
+++ scripts/hud/health_bar.gd
@@ -0,0 +1,21 @@
+extends Control
+
+@export_category("Health")
+@export var max_health: float
+
+var health: float
+var health_bar: ColorRect
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	health = max_health
+	health_bar = $Foreground
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	health_bar.scale.x = health / max_health
+
+# Returns true if the entity should die
+func take_damage(damage: float):
+	health -= damage
+	return health <= 0
